a49c2f0916727bfc9b15075438945ed231ef5bba
philip-webdev <philisobank@gmail.com>
2024-10-08 16:52:26 +0100

    news

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,17 @@ import myProfile from './components/avatar';
 import checkout from "./components/checkout";
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
 import "./index.css";
+import  './manifest.json';
 // this manifest is used temporarily for development purposes
 const manifestUrl =
   "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
-
+ const newurl =  'https://github.com/Philip-webdev/telegram-webApp/blob/main/manifest.json'
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
+  <TonConnectUIProvider manifestUrl={newurl}>
     <QueryClientProvider client={queryClient}>
      
       <App />
