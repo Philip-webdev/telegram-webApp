54b922c108e720c19da596e73a4b45f7c205041e
Philip-webdev <106782051+Philip-webdev@users.noreply.github.com>
2024-10-08 16:46:20 +0000

    Deploying to gh-pages from @ Philip-webdev/telegram-webApp@00d5901bafc74954fdb07f4a3aa0c9fcb16b6d80 🚀

--- assets/index-4f9e160a.js
+++ assets/index-4f9e160a.js
@@ -1255,7 +1255,7 @@ Values:
   color: black;
 
   @media (prefers-color-scheme: dark) {
-    background-color: #222;
+    background-color: rgb(29, 40, 58);
     color: white;
   }
   min-height: 90vh;
