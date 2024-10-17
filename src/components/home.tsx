a957518f0d1b4f0e43322c0f19a84c5494fe8f1f
Philip <106782051+Philip-webdev@users.noreply.github.com>
2024-10-17 14:26:09 +0100

    Update home.tsx

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -243,54 +243,55 @@ return(
 <img style={{margin:'5.5px', zoom:'70%'}} height='17px' width='17px' src=' https://ouch-cdn2.icons8.com/BeHnWh41tAHTg1zxTla1u36mz_qQqLaKI-W37IVUqE4/rs:fit:368:687/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNDI3/LzBjYjdjOTJlLTBj/ZDEtNGY4Ni1hNTli/LWFkYWZmODcxNDY3/Yy5zdmc.png'/>
 
 <div style={{display:'flex',zoom:'45%', justifyContent:'space-evenly'}}><BsHeart style={{zoom:'50%', color:'black', marginBottom:'1px'}}/> <BsPlus style={{zoom:'50%', marginBottom:'1px',color:'black'}}/></div></div>
-<div  onClick={saveProduct2} id='prod_two'className="items" style={{borderRadius:'5px', margin:'6px', height: '20px', width: '150px', backgroundColor: '#e8e8e8'}}>
+<div  onClick={saveProduct2} id='prod_two'className="items" style={{borderRadius:'5px', margin:'6px', height: '20px', width: '70px', backgroundColor: '#e8e8e8'}}>
 <caption  style={{zoom:'15%', color:'black', marginLeft:'27px', cursor:'pointer'}}>pear</caption>
     <img style={{margin:'5.5px', zoom:'70%'}} height='17px' width='17px' src='   https://ouch-cdn2.icons8.com/9J-tqJWz53hWH0mo75-dhyg9cVrv3rNPoUfGLFVaw_k/rs:fit:368:143/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTQv/NDBlNGFkMjYtOTA2/ZS00MWM1LTllNjQt/OTk0Y2UzMWJkNjM1/LnN2Zw.png'/>
    </div>
-<div  onClick={saveProduct3} id='prod_three' className="items" style={{borderRadius:'5px', margin:'6px', height: '20px', width: '150px', backgroundColor: '#e8e8e8'}}>
+<div  onClick={saveProduct3} id='prod_three' className="items" style={{borderRadius:'5px', margin:'6px', height: '20px', width: '70px', backgroundColor: '#e8e8e8'}}>
 <caption style={{zoom:'15%', color:'black', marginLeft:'4px', cursor:'pointer'}}>pear</caption>
     <img style={{margin:'5.5px', zoom:'70%'}} height='17px' width='17px' src=' https://ouch-cdn2.icons8.com/ET5TgmBkf_yeuGltz9l7yp01wtX9aJMD-Nh8zz4UphY/rs:fit:368:320/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMzQ4/L2RkNzk5ZTczLWJj/YTEtNDBlMC04NmNj/LTU4Nzk5NzVkYjdl/YS5wbmc.png'/>
    </div></section>
     
 
-<section style={{display:'inline',zoom:'330%'}}><div onClick={saveProduct4} id='prod_four' className="items" style={{borderRadius:'5px',margin:'6px',  height: '20px', width: '150px', backgroundColor: '#e8e8e8'}}>
+<section style={{display:'inline',zoom:'330%'}}><div onClick={saveProduct4} id='prod_four' className="items" style={{borderRadius:'5px',margin:'6px',  height: '20px', width: 70px', backgroundColor: '#e8e8e8'}}>
 <caption  style={{zoom:'15%', color:'black', marginLeft:'27px', cursor:'pointer'}}>pear</caption>
 
   <img style={{margin:'5.5px', zoom:'70%'}} height='17px' width='17px' src=' https://ouch-cdn2.icons8.com/5cIK3aleg5XZUrrfkNJOBKzFMYxtpOXeo181NvzVob8/rs:fit:368:442/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMzY0/L2I1MDhhMDU0LTU3/ZWItNDJhYS1iY2Iz/LWVkYWU2OTRmOGU5/ZC5wbmc.png'/>
    
     <div style={{display:'flex', zoom:'50%', justifyContent:'space-evenly'}}><BsHeart style={{zoom:'50%', color:'black',marginBottom:"1px"}}/> <BsPlus style={{zoom:'50%',marginBottom:"1px",color:'black'}}/></div></div>
-<div onClick={saveProduct5} id='prod_five' className="items" style={{borderRadius:'5px',margin:'6px',  height: '20px', width: '150px', backgroundColor: '#e8e8e8'}}>
+<div onClick={saveProduct5} id='prod_five' className="items" style={{borderRadius:'5px',margin:'6px',  height: '20px', width: '70px', backgroundColor: '#e8e8e8'}}>
 <caption  style={{zoom:'15%', color:'black', marginLeft:'27px', cursor:'pointer'}}>pear</caption>
     <img style={{margin:'5.5px', zoom:'70%'}} height='17px' width='17px' src=' https://ouch-cdn2.icons8.com/4E_6STmrlhph_nKBUI8Tv5Pr9TGiE74C4w6cB3Pzh3w/rs:fit:368:317/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNjAx/LzZiMGUyMGMzLTIy/MTEtNDg0OS05MDMy/LTUxNWZjMjljMTQ1/My5zdmc.png'/>
 </div>
-<div onClick={saveProduct6} id='prod_six' className="items" style={{borderRadius:'5px', margin:'6px', height: '20px', width: '150px', backgroundColor: '#e8e8e8'}}>
+<div onClick={saveProduct6} id='prod_six' className="items" style={{borderRadius:'5px', margin:'6px', height: '20px', width: '70px', backgroundColor: '#e8e8e8'}}>
 <caption  style={{zoom:'15%', color:'black', marginLeft:'27px', cursor:'pointer'}}>pear</caption>
     <img style={{margin:'5.5px', zoom:'70%'}} height='17px' width='17px' src=' https://ouch-cdn2.icons8.com/4w-wbnJaspImr0-rImGetVrt_UGrTVPKkYj_GdWx6VU/rs:fit:368:484/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTUz/LzM4MmFlMGRhLTlj/OGYtNDcwNy1iZDAz/LWQyOTJlZDc3M2Yw/NC5wbmc.png'/>
    </div>
 </section>
 <section style={{display:'inline', zoom:'330%'}}>
-<div onClick={saveProduct7} id='prod_seven' className="items" style={{borderRadius:'5px',margin:'6px',  height: '20px', width: '150px', backgroundColor: '#e8e8e8'}}>
+<div onClick={saveProduct7} id='prod_seven' className="items" style={{borderRadius:'5px',margin:'6px',  height: '20px', width: '70px', backgroundColor: '#e8e8e8'}}>
 <caption  style={{zoom:'15%', color:'black', marginLeft:'27px', cursor:'pointer'}}>pear</caption>
     <img style={{margin:'5.5px', zoom:'70%'}} height='17px' width='17px' src=' https://ouch-cdn2.icons8.com/BeHnWh41tAHTg1zxTla1u36mz_qQqLaKI-W37IVUqE4/rs:fit:368:687/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNDI3/LzBjYjdjOTJlLTBj/ZDEtNGY4Ni1hNTli/LWFkYWZmODcxNDY3/Yy5zdmc.png'/>
    
  <div style={{display:'flex', zoom:'50%', justifyContent:'space-evenly'}}><BsHeart style={{zoom:'50%', color:'black',marginBottom:"1px"}}/> <BsPlus style={{zoom:'50%',marginBottom:"1px",color:'black'}}/></div></div>
-<div onClick={saveProduct8} id='prod_eight' className="items" style={{borderRadius:'5px', margin:'6px', height: '20px', width: '150px', backgroundColor: '#e8e8e8'}}>
+<div onClick={saveProduct8} id='prod_eight' className="items" style={{borderRadius:'5px', margin:'6px', height: '20px', width: '70px', backgroundColor: '#e8e8e8'}}>
 <caption  style={{zoom:'15%',  color:'black', marginLeft:'27px', cursor:'pointer'}}>pear</caption>   <img style={{margin:'5.5px', zoom:'70%'}} height='17px' width='17px' src='   https://ouch-cdn2.icons8.com/9J-tqJWz53hWH0mo75-dhyg9cVrv3rNPoUfGLFVaw_k/rs:fit:368:143/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvOTQv/NDBlNGFkMjYtOTA2/ZS00MWM1LTllNjQt/OTk0Y2UzMWJkNjM1/LnN2Zw.png'/>
     </div>
-<div onClick={saveProduct9} id='prod_nine' className="items" style={{borderRadius:'5px', margin:'6px', height: '20px', width: '150px', backgroundColor: '#e8e8e8'}}>
+<div onClick={saveProduct9} id='prod_nine' className="items" style={{borderRadius:'5px', margin:'6px', height: '20px', width: '70px', backgroundColor: '#e8e8e8'}}>
 <caption  style={{zoom:'15%', color:'black', marginLeft:'27px', cursor:'pointer'}}>pear</caption>
  <img style={{margin:'5.5px', zoom:'70%'}} height='17px' width='17px' src=' https://ouch-cdn2.icons8.com/ET5TgmBkf_yeuGltz9l7yp01wtX9aJMD-Nh8zz4UphY/rs:fit:368:320/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvMzQ4/L2RkNzk5ZTczLWJj/YTEtNDBlMC04NmNj/LTU4Nzk5NzVkYjdl/YS5wbmc.png'/>
   </div>
 </section>
 
 <section style={{display:'inline', zoom:'330%'}}>
-<div className="items" style={{borderRadius:'5px',margin:'6px', height: '20px', width: '20px', backgroundColor: '#e8e8e8'}}>
+<div className="items" style={{borderRadius:'5px',margin:'6px', height: '20px', width: '70px', backgroundColor: '#e8e8e8'}}>
     <img style={{margin:'5.5px', zoom:'70%'}} height='17px' width='17px' src=' https://ouch-cdn2.icons8.com/4E_6STmrlhph_nKBUI8Tv5Pr9TGiE74C4w6cB3Pzh3w/rs:fit:368:317/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNjAx/LzZiMGUyMGMzLTIy/MTEtNDg0OS05MDMy/LTUxNWZjMjljMTQ1/My5zdmc.png'/>
     <div style={{display:'flex',zoom:'45%', justifyContent:'space-evenly'}}>
         <BsHeart style={{zoom:'50%', color:'black',marginBottom:"1px"}}/> <BsPlus style={{zoom:'50%',marginBottom:"1px",color:'black'}}/>
         </div>
 </div>
-<div className="items" style={{borderRadius:'6px', margin:'5px', height: '20px', width: '250px', backgroundColor: '#e8e8e8'}}>
+<div className="items" style={{borderRadius:'6px', margin:'5px', height: '20px', width: '25
+70px', backgroundColor: '#e8e8e8'}}>
     <img style={{margin:'5.5px', zoom:'70%'}} height='17px' width='17px' src=' https://ouch-cdn2.icons8.com/4E_6STmrlhph_nKBUI8Tv5Pr9TGiE74C4w6cB3Pzh3w/rs:fit:368:317/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNjAx/LzZiMGUyMGMzLTIy/MTEtNDg0OS05MDMy/LTUxNWZjMjljMTQ1/My5zdmc.png'/>
     <div style={{display:'flex',zoom:'45%', justifyContent:'space-evenly'}}>
         <BsHeart style={{zoom:'50%', color:'black',marginBottom:"1px"}}/> <BsPlus style={{zoom:'50%',marginBottom:"1px",color:'black'}}/></div></div>
